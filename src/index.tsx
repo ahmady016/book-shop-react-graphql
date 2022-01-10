0a0afb0e54ae28fd80a74251b4bdb56602edf6a7
ahmady016 <ahmady09@gmail.com>
2022-01-11 01:10:01 +0200

    Add GlobalStyles, BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+import { BrowserRouter } from 'react-router-dom'
+import { GlobalStyles } from 'twin.macro'
+
 import App from './App'
 import './index.css'
 
 render(
 	<React.StrictMode>
-		<App />
+		<GlobalStyles />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById('root')
 )
